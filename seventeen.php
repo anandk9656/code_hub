ecc20a58acda9d05a8aeb52d25443d07db04b622
ANAND KUMAR <anandk9656@gmail.com>
2017-03-21 10:16:49 +0530

    static function

---
+++ seventeen.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'head.html';
+    echo "<h1> <----: Static function inside the class :----> </h1> ";
+    class employee
+    {
+       public $num1,$num2;
+        static function show()
+        {
+            echo "<h2> <i> This is an employee class </i> </h2>";
+        }
+    }
+    employee::show();
+   
+    echo"<br> <br>";
+    
+    class declre
+    {
+           // class with no properties and methods.
+    }
+    $d1=new declre();
+    $d1->name="ANAND";   // implicitly creating of the properties of a class.   
+    $d1->Emp_code="0035";
+    echo $d1->name;
+    echo"<br>";
+    echo $d1->Emp_code;
+    
+?>
+
