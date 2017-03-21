fc7b5f24e9fa393ee42271b00848958d81e91405
ANAND KUMAR <anandk9656@gmail.com>
2017-03-21 10:12:36 +0530

    Typecasting

---
+++ thirteen.php
@@ -0,0 +1,10 @@
+<?php
+    echo"<h1> <----: Type Casting ( Implicit and Explicit ) :----> </h1>";
+    $a=100;
+    $b=3;
+    echo "Implicit type conversion :  ". $c= ($a/$b);   // Implicit type conversion. 
+    echo"<br> <br />";
+    echo "Explicit type conversion :  ". $d= (int) ($a/$b);   // Explicit type conversion.
+?>
+
+
