f4efde58555a18cd6e03d4d9dbcad650198b7df0
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:53:47 +0530

    Using Final Keyword

--- twentytwo.php
+++ twentytwo.php
@@ -1,30 +1,23 @@
 <?php
     include 'head.html';
-    echo"<h1> <---: final keyword in php :---> </h1>";
-    class admin
-    {
-    final  function show()
-        {
+    echo"<h1>Final keyword in php</h1>";
+    class admin{
+    final function show(){  // remove final to see working
             echo "This is show method of admin class (parent class) ";
             echo"<br>";
         }
     }
-    class user extends admin
-    {
-        function show()
-        {
-              echo"This is show method of user class (child class)";
-              echo"<br>";
+    class user extends admin{
+        function show(){
+              echo"This is <b>show method</b> of user class (child class)";
         }
-//        function aces()    // this function is used to access the show() function of parent class.
-//        {
-//            parent::show();    // syntax of how to use the parent class method.
-//        }
+/*        function aces()    // this function is used to access the show() function of parent class.
+        {
+           parent::show();    // syntax of how to use the parent class method.
+        }
+ */
     }
     $u=new user;
     $u->show();  
-    // $u->aces();
-   //  $u->aces(); used to acces the parent class method by the object of child class with same function name.
-    
-?>
-
+   // $u->aces(); used to acces the parent class method by the object of child class with same function name.
+   include 'foot.html';
