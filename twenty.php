9f01784b8e9b063c7fae108842b681c0a854955d
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:51:53 +0530

    Constructor And Destructor

--- twenty.php
+++ twenty.php
@@ -1,45 +1,36 @@
 <?php
 include 'head.html';    
-    echo"<h1> <----: Constructor & Destructor in php :----> </h1>";
+    echo"<h1>Constructor & Destructor in php</h1>";
     echo"<h2> Default constructor in php </h2>";
-    class defaut
-    {
+    class defaut{
         public $name="Anand Kumar";
-                function __construct() 
-                {
-                        echo"This is a default constructor";
+                function __construct() {
+                        echo"This is a <b>default constructor</b>";
                         echo "<br>";
                 }
-                function put()
-                {
-                    echo"This is put function, "."My name is : ".$this->name;
+                function put(){
+                    echo"This is <b>put function</b>, "."My name is : ".$this->name;
                 }
     }
     $d1=new defaut();
     $d1->put();
-    
-    echo"<br><br>";
+    echo"<br>";
     
     echo"<h2> Parameterized Constructor in php </h2>"; 
-    
-    class user
-    {
+    class user{
         public $a,$b;
-        function __construct($a,$b) 
-        {
-            echo"This is a Parameterized constructor";
+        function __construct($a,$b){
+            echo"This is a <b>Parameterized constructor</b>";
             echo "<br>";
             echo $this->a=$a;
             echo"<br>";
             echo $this->b=$b;
             echo"<br>";
         }
-        function show()
-        {
-            echo "This is show function";
+        function show(){
+            echo "This is <b>show function</b>";
         }
     }
  $obj = new user(12,15);
- $obj->show();   
-?>
-
+ $obj->show();
+ include 'foot.html';
