d70c8f0020a75b028f729058fda2eea7dd6c9c71
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:35:48 +0530

    One_Dimensional_Array

--- three.php
+++ three.php
@@ -1,6 +1,7 @@
+<style>body{ text-align: center; }</style>
 <?php
     // One-dimensional Array
-    echo" <h1> <---:  One_Dimensional_Array<br><br> :----> </h1>  ";
+    echo "<h1><---: One_Dimensional_Array :----></h1>";
     
     $student = array("ANAND","SHUBHAM","RAVI","ARUN","SUMAN",1,2,3,4,5,6,7,8,9,0);
 //    echo $student[4]; 
@@ -9,12 +10,8 @@
 //    echo $student[2];
 //    echo"<br> <br>";
     echo'<pre>';
-//    print_r($student); // it will print whole array value with their subscript.
-   echo $count = count($student)."<br>";
-   
-    for($i=$count-1;$i>=$count-10;$i--)
-    {
+   // print_r($student); // it will print whole array value with their subscript.
+    $count = count($student);  // Returns the length of the array. 
+    for($i=0;$i<$count;$i++){
         echo $student[$i]."<br>";
     }
-?>
-
