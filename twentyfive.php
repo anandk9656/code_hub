9ce4b25dd0a5d769570351d2ba1dbc183b20df6a
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:30:53 +0530

    Form Handling

--- twentyfive.php
+++ twentyfive.php
@@ -1,35 +1,26 @@
 <?php
 include 'head.html';
-echo "<h1> <----:Form Handling in php :----> </h1>";
-if(isset($_POST['uname'])) 
-    {
-        $n = $_POST['uname'];
-    }
-    echo"If you enter any name then it will be displayed."."<br> <br>";
+echo "<h1>Form Handling in php</h1>";
+    echo"If you enter any name then it will be displayed."."<br>";
     echo"If not enter any name then error wil be shown.";
 ?>
-   
-    <html> 
+<html> 
         <head> <title> Form Handling </title> </head>
     <body>
-        <form method="post" action="twentyfive.php">
+        <form method="post" action=<?php $_SERVER['PHP_SELF']?>>
             <pre>
-            Name: <input type="text" name ="uname" placeholder="enter name"> <br /> <br />
-          
-            <input type="submit" value="print"> <br /> <br />
-            Name : <br>
-            <?php if(!empty($n)) 
-                   { 
-                        echo $n; 
-                   }
-                   else
-                   {
-                       echo" !!!! error error error ";
-                   }     
+            Name: <input type="text" name ="uname" placeholder="Enter name"> <br />
+            <input type="submit" value="print" name="done"> <br />
+            <?php
+            if(isset($_POST['done'])) {
+            if(!empty($_POST['uname'])){ 
+                      echo "Name you have entered is : ".$_POST['uname']; 
+            }else{
+                    echo 'Enter name first then try to print';  
+                 }   
+            }
             ?> 
             </pre>
         </form>
     </body>
-   </html>
-
-
+</html> <?php include 'foot.html';
