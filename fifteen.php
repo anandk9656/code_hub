2e876bc919e39f57666282d507eaeda95391e65a
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:47:47 +0530

    Include And Require Once

--- fifteen.php
+++ fifteen.php
@@ -1,17 +1,12 @@
 <?php
     include_once  'head.html';
-    echo" <h1> <----: Include and Require, Once :----> </h1>";
-    include 'eight.php';
-   
-    
+    echo" <h1>Include and Require, Once</h1> ";
+    include 'eight.php';  
     echo "<h2><i>Checking the existence of the function by using ( function_exists ) in if-else condition.</i> </h2>";
-    if (function_exists("array_combine"))
-    {
+    if (function_exists("parse_strs")){
         echo "Function exists";
-    }   
-    else
-    {
+    } else{
         echo "Function does not exist - better write your own";
     }
    include_once 'eight.php';
-    include 'foot.html';
+   include 'foot.html';
