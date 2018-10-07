f2eda745548d671f52c84a64562e2a76bc381239
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:37:27 +0530

    Multidimensional_Array

--- four.php
+++ four.php
@@ -1,6 +1,6 @@
 <?php
     // Two-dimensional Array
-     echo" <h1> <----: Two_Dimensional_Array<br><br> :----> </h1> ";
+     echo" <h1> <----: Two_Dimensional_Array :----> </h1> ";
      
      $college = array(
                         array(1,"Anand","Physics"),
@@ -8,9 +8,47 @@
                         array(3,"Ravi","Mathematics")
                     );
      
-     echo $college[1][2];
+     echo "SecondRow__ThirdData : ".$college[1][2]; // Indexing starts from 0 (not1).
      
-     echo"<br> <br>";
+     echo"<br>";
      echo'<pre>';
      print_r($college);
-?>
+     echo'</pre><br>';
+     $student = array(
+         array(
+             "No"=>'B',
+             array(
+                 "id"=>"000",
+                 "Name"=>"KV AFS",
+                 "Code"=>122
+                  )
+         ),
+         array(
+             "No"=>'A',
+             array(
+                  "id"=>"001",
+                 "Name"=>"KV AFS",
+                 "Code"=>122
+             )
+         ),
+         array(
+             "No"=>'C',
+             array(
+                  "id"=>"002",
+                 "Name"=>"KGK AFS",
+                 "Code"=>124352
+             )
+         ),
+         array(
+             "No"=>'D',
+             array(
+                  "id"=>"003",
+                 "Name"=>"JKAjk",
+                 "Code"=>16722
+             )
+         )
+     );
+     echo'<pre>';
+    // print_r($student);
+     echo'</pre><br>';
+     echo $student[1][0]['id'];
