cea7e5c6109e53b92636324424fc361e8e0a38f6
Anand Kumar <anandk9656@gmail.com>
2018-10-07 15:46:51 +0530

    Predefined Functions

--- fourteen.php
+++ fourteen.php
@@ -18,37 +18,37 @@
     echo"<----: print function :---->"."<br> <br>";
     print(10-4)." : is (10-4)"; echo"<br>";
     print (sqrt(625))." :  is square root of 625";
-    echo"<br> <br>";
+    echo"<br>";
     
-    echo"<h3><u>Function with single return :</u> </h3>"."<br> ";
+    echo"<h3><u>Function with single return :</u> </h3>";
     $n1="aNanD";
     $n2="ShuBhaM";
     $n3="subrAT";
     function name($n1,$n2,$n3)
     {
         $n1=  ucfirst(strtolower($n1));
-         $n2=  ucfirst(strtolower($n2));
-          $n3=  ucfirst(strtolower($n3));
-          return $n1." ".$n2." ". $n3;
+        $n2=  ucfirst(strtolower($n2));
+        $n3=  ucfirst(strtolower($n3));
+        return $n1." ".$n2." ". $n3;
     }
     echo name($n1, $n2, $n3);
-    echo"<br> <br>";
+    echo"<br>";
     
-    echo"<h3><u>Function with multiple  return as array :</u> </h3>"."<br>"; 
+    echo"<h3><u>Function with multiple  return as array :</u> </h3>"; 
     $n1="aNanD";
     $n2="ShuBhaM";
     $n3="subrAT";
     function names($a,$b,$c)
     {
         $a=  ucfirst(strtolower($a));
-         $b=  ucfirst(strtolower($b));
-          $c=  ucfirst(strtolower($c));
+        $b=  ucfirst(strtolower($b));
+        $c=  ucfirst(strtolower($c));
           return array($a,$b,$c);
     }
     $arr= names($n1, $n2, $n3);
     echo $arr[1]." ". $arr[0];
  
-    echo"<br> <br>";
+    echo"<br>";
     echo"<h3>Using Global values in function by global keyword</h3> ";
     $a=10;
     $b=20;
@@ -63,8 +63,8 @@
     sum(); echo"<br>";
     echo $sum;
     
-    echo "<br> <br>";
-    echo "<h3> Returning global variables </h3>"."<br>";
+    echo "<br>";
+    echo "<h3> Returning global variables </h3>";
     $x=10;
     $y=20;
     $z=30;
@@ -77,4 +77,3 @@
         global $y; ++$y;
         global $z; ++$z;
     }
-?>
